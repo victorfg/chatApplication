186e2035906e67d063fcd6b68627e24e5bc3c017
Manuel <m.mendizabal@mail.zeus.vision>
2020-05-31 20:53:08 +0200

    crud in process

--- server/index.js
+++ server/index.js
@@ -2,22 +2,31 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
-const PORT = process.env.PORT || 5000;
-
+const config = require('./config');
 const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket)=> {
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//Db Connection Start
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URL, { useNewPassword: true, password: process.env.MONGODB_PASSWORD, useNewUrlParser: true })
+    .then(() => console.log('connection succesful'))
+    .catch((err) => console.error(err));
+//Db Connetion End
+
+io.on('connection', (socket) => {
     console.log('socketIO tenemos nueva conexion!!');
 
-    socket.on ('disconnect', () => {
+    socket.on('disconnect', () => {
         console.log('el usuario se ha ido!!')
     })
 })
 
 app.use(router);
 
-server.listen(PORT);
+server.listen(process.env.SERVER_PORT);
